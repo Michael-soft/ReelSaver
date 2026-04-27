363383650ff7b4f84247ad2c4eecc856e6cea800
@Akinde_Tosin <tosin.akinde36@gmail.com>
2026-04-27 01:08:33 +0100

    Switch DB to Supabase: prefer SUPABASE_DATABASE_URL with SSL + pool config

--- server/app.py
+++ server/app.py
@@ -23,12 +23,22 @@ app = Flask(
 app.secret_key = os.environ.get("SESSION_SECRET")
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+_db_url = os.environ.get("SUPABASE_DATABASE_URL") or os.environ.get("DATABASE_URL")
+if _db_url and _db_url.startswith("postgres://"):
+    _db_url = "postgresql://" + _db_url[len("postgres://"):]
+app.config["SQLALCHEMY_DATABASE_URI"] = _db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     'pool_pre_ping': True,
-    "pool_recycle": 300,
+    'pool_recycle': 300,
+    'pool_size': 5,
+    'max_overflow': 5,
+    'connect_args': {
+        'sslmode': 'require',
+        'connect_timeout': 10,
+    },
 }
+logging.info("DB host: %s", (_db_url.split('@', 1)[1].split('/', 1)[0] if _db_url and '@' in _db_url else 'unknown'))
 
 db = SQLAlchemy(app, model_class=Base)
 
